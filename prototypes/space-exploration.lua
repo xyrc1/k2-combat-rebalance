7ea2959ad6e476b7d11b6004f2e39e331e71f890
xyrc1 <75494811+xyrc1@users.noreply.github.com>
2026-08-02 13:23:58 -0700

    disable energy shield rebal when SE installed

---
+++ prototypes/space-exploration.lua
@@ -0,0 +1,4 @@
+if not mods['space-exploration'] then return end
+
+data.raw['bool-setting']['xy-rebalance-energy-shield'].hidden = true
+data.raw['bool-setting']['xy-rebalance-energy-shield'].default_value = false
